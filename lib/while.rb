5a987027193626f8bd6cf1c24597511da9174d05
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-09 02:12:06 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,11 @@
+#input puts(phrase) while >10 put incrment force by 1 
+
+
 def using_while
   levitation_force = 6
-	
-  #your code here
-end
-
+  while levitation_force <10 
+    puts "Wingardium Leviosa"
+    levitation_force +=1
+  end 
+end 
 
